537905a600c213fab50340fe541b36b8ce085025
craigraphics <craigraphics@mail.com>
2024-06-24 14:44:09 -0700

    added mock API to project

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -30,11 +30,11 @@ export class ReservationFormComponent implements OnInit {
     let id = this.activatedRoute.snapshot.paramMap.get('id');
 
     if (id) {
-      let reservation = this.reservationService.getReservation(id);
-
-      if (reservation) {
-        this.reservationForm.patchValue(reservation);
-      }
+      this.reservationService.getReservation(id).subscribe((reservation) => {
+        if (reservation) {
+          this.reservationForm.patchValue(reservation);
+        }
+      });
     }
   }
 
@@ -46,10 +46,14 @@ export class ReservationFormComponent implements OnInit {
 
       if (id) {
         // update
-        this.reservationService.updateReservation(id, reservation);
+        this.reservationService
+          .updateReservation(id, reservation)
+          .subscribe(() => console.log('update reservation processed'));
       } else {
         // new
-        this.reservationService.addReservation(reservation);
+        this.reservationService
+          .addReservation(reservation)
+          .subscribe(() => console.log('new reservation processed'));
       }
 
       this.router.navigate(['/list']);
